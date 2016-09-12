ba544b1e8d27ae81dabe285a240e500c9a956ab9
mmurphy <mmurphy@omniti.com>
2016-09-12 15:25:45 +0000

    pg_partman version bump

--- build/pg95-pg_partman/build.sh
+++ build/pg95-pg_partman/build.sh
@@ -28,9 +28,9 @@
 . ../../lib/functions.sh
 
 PROG=pg_partman
-VER=2.3.4
+VER=2.6.0
 VERHUMAN=$VER
-: ${PGVER:=953}
+: ${PGVER:=954}
 PKG=omniti/database/postgresql-${PGVER}/pg_partman
 SUMMARY="$PROG - Partition management extention for PostgreSQL"
 DESC="$SUMMARY"
