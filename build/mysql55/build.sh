a3ec398cc286eb8f3d4610e1701da98dfcd39dfd
mmurphy <mmurphy@omniti.com>
2018-01-24 21:25:43 +0000

    update mysql

--- build/mysql55/build.sh
+++ build/mysql55/build.sh
@@ -28,7 +28,7 @@
 . ../../lib/functions.sh
 
 PROG=mysql
-VER=5.5.58
+VER=5.5.59
 VERHUMAN=$VER
 
 BUILD_DEPENDS_IPS="omniti/developer/build/cmake system/library/g++-4-runtime system/library/gcc-4-runtime"
